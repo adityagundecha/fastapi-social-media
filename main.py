6794bb258d80dfbcb9cc754a0d0766c77b0c24a6
Aditya Gundecha <aditya.gundecha@siemens.com>
2021-12-20 13:14:21 +0530

    added more crud methods and in memory posts db

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
@@ -13,16 +14,32 @@ class Post(BaseModel):
     rating: Optional[int] = None
 
 
+my_posts = [
+    {"title": "title of post 1", "content": "content of post 1", "id": 1},
+    {"title": "favourite foods", "content": "I like Pizza", "id": 2}
+]
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 
 @app.get("/posts")
-def get_post():
-    return {"data": "This is a post"}
+def get_posts():
+    return {"data": my_posts}
+
+
+@app.get("/posts/{id}")
+def get_post(id):
+    for post in my_posts:
+        if post['id'] == int(id):
+            return post
 
 
 @app.post("/posts")
 def createPost(post: Post):
-    return {"data": post}
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return post_dict
