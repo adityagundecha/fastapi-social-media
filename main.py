e94e7e9f65b3496f822f7faecc5ec60c7edfbba9
Aditya Gundecha <aditya.gundecha@siemens.com>
2021-12-20 20:01:43 +0530

    CRUD operations completed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
@@ -30,16 +30,42 @@ def get_posts():
     return {"data": my_posts}
 
 
-@app.get("/posts/{id}")
-def get_post(id):
-    for post in my_posts:
-        if post['id'] == int(id):
-            return post
-
-
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def createPost(post: Post):
     post_dict = post.dict()
     post_dict['id'] = randrange(0, 1000000)
     my_posts.append(post_dict)
     return post_dict
+
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    for post in my_posts:
+        if post['id'] == id:
+            return post
+    raise HTTPException(status.HTTP_404_NOT_FOUND,
+                        detail=f"Post with id: {id} not found")
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    for post in my_posts:
+        if post['id'] == id:
+            my_posts.remove(post)
+            return Response(status_code=status.HTTP_204_NO_CONTENT)
+    raise HTTPException(status.HTTP_404_NOT_FOUND,
+                        detail=f"Post with id: {id} not found")
+
+
+@app.put("/posts/{id}")
+def update_post(id: int, my_post: Post):
+    post_dict = my_post.dict()
+    post_dict['id'] = id
+    print(post_dict)
+    for index, post in enumerate(my_posts):
+        print(id)
+        if post['id'] == id:
+            my_posts[index] = post_dict
+            return {"data": post_dict}
+    raise HTTPException(status.HTTP_404_NOT_FOUND,
+                        detail=f"Post with id {id} not found")
