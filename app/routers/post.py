1f19251b0e436a9ee11a93d8977335b8a1332049
Aditya Gundecha <adityagundecha@hotmail.com>
2022-01-13 23:43:25 +0530

    Adding limit functionality to get all route

--- app/routers/post.py
+++ app/routers/post.py
@@ -18,11 +18,11 @@ router = APIRouter(
 
 
 @router.get("/", response_model=List[schemas.PostResponse])
-def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), limit: int = 10):
     # cursor.execute("""SELECT * FROM posts""")
     # posts = cursor.fetchall()
     # print(current_user)
-    posts = db.query(models.Post).all()
+    posts = db.query(models.Post).limit(limit).all()
     return posts
 
 
