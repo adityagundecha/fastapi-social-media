70239b6b545e1dd6d42fc8cc18b2154b351de8c8
Aditya Gundecha <adityagundecha@hotmail.com>
2022-01-13 23:53:30 +0530

    Cleanup main file

--- app/database.py
+++ app/database.py
@@ -4,6 +4,10 @@ from sqlalchemy.orm import sessionmaker
 from os.path import join, dirname
 from dotenv import load_dotenv
 import os
+import psycopg2
+import time
+from os.path import dirname, join
+from psycopg2.extras import RealDictCursor
 
 dotenv_path = join(dirname(__file__), '../.env')
 load_dotenv(dotenv_path)
@@ -12,6 +16,18 @@ HOST = os.environ.get("host")
 DATABASE = os.environ.get("database")
 USER = os.environ.get("user")
 PASSWORD = os.environ.get("password")
+
+# Connect to the db
+while True:
+    try:
+        conn = psycopg2.connect(host=HOST, database=DATABASE,
+                                user=USER, password=PASSWORD, cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print('DB Conn was successfull!')
+        break
+    except Exception as error:
+        print(f"Error: {error}")
+        time.sleep(2)
 
 
 SQLALCHEMY_DATABASE_URL = f'postgresql://{USER}:{PASSWORD}@{HOST}/{DATABASE}'
--- app/main.py
+++ app/main.py
@@ -1,12 +1,4 @@
-import os
-import time
-from os.path import dirname, join
-
-import psycopg2
-from dotenv import load_dotenv
 from fastapi import FastAPI
-from psycopg2.extras import RealDictCursor
-
 from . import models
 from .database import engine, get_db
 from .routers import post, user, auth
@@ -14,29 +6,6 @@ from .routers import post, user, auth
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 get_db()
-
-
-# Get the database parameters from env
-dotenv_path = join(dirname(__file__), '../.env')
-load_dotenv(dotenv_path)
-
-HOST = os.environ.get("host")
-DATABASE = os.environ.get("database")
-USER = os.environ.get("user")
-PASSWORD = os.environ.get("password")
-
-
-# Connect to the db
-while True:
-    try:
-        conn = psycopg2.connect(host=HOST, database=DATABASE,
-                                user=USER, password=PASSWORD, cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print('DB Conn was successfull!')
-        break
-    except Exception as error:
-        print(f"Error: {error}")
-        time.sleep(2)
 
 
 # Load routers
