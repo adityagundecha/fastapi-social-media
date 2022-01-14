3acfa1208390e48c166e2823d025c3ac46f03deb
Aditya Gundecha <adityagundecha@hotmail.com>
2022-01-15 00:00:56 +0530

    Adding Vote functionality

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from tkinter import CASCADE
 from sqlalchemy.orm import relation
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
@@ -25,3 +26,11 @@ class User(Base):
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text('now()'))
+
+
+class Vote(Base):
+    __tablename__ = "votes"
+    user_id = Column(Integer, ForeignKey(
+        "users.id", ondelete=CASCADE), primary_key=True)
+    post_id = Column(Integer, ForeignKey(
+        "posts.id", ondelete=CASCADE), primary_key=True)
---
+++ app/routers/vote.py
@@ -0,0 +1,37 @@
+from fastapi import APIRouter, HTTPException, status
+from fastapi.params import Depends
+from sqlalchemy.orm import Session
+from starlette.responses import Response
+from starlette.status import HTTP_403_FORBIDDEN
+
+from app import oauth2
+
+from .. import models, schemas, oauth2, database
+from ..database import get_db
+
+router = APIRouter(
+    prefix="/vote",
+    tags=['Vote']
+)
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+def vote(vote: schemas.Vote, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+    vote_query = db.query(models.Vote).filter(
+        models.Vote.post_id == vote.post_id, models.Vote.user_id == current_user.id)
+    found_vote = vote_query.first()
+    if (vote.dir) == 1:
+        if(found_vote):
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                                detail=f"User {current_user.id} cannot like the post {vote.post_id} again")
+        new_vote = models.Vote(post_id=vote.post_id, user_id=vote.user_id)
+        db.add(new_vote)
+        db.commit()
+        return {"message": "Vote added successfully"}
+    else:
+        if not found_vote:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail=f"User {current_user.id} cannot unlike the post {vote.post_id} without liking it first")
+        vote_query.delete(synchronize_session=False)
+        db.commit()
+        return {"message": "Vote added successfully"}
