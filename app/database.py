3a6425a7c27fdfab90191a6e55cecc71d18857d3
Aditya Gundecha <adityagundecha@hotmail.com>
2022-01-14 23:36:14 +0530

    Code cleanup

--- app/database.py
+++ app/database.py
@@ -4,12 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from os.path import join, dirname
 from dotenv import load_dotenv
 import os
-import psycopg2
-import time
 from os.path import dirname, join
-from psycopg2.extras import RealDictCursor
-from .database import engine, get_db
-from . import models
 
 dotenv_path = join(dirname(__file__), '../.env')
 load_dotenv(dotenv_path)
@@ -20,25 +15,23 @@ USER = os.environ.get("user")
 PASSWORD = os.environ.get("password")
 
 # Connect to the db
-while True:
-    try:
-        conn = psycopg2.connect(host=HOST, database=DATABASE,
-                                user=USER, password=PASSWORD, cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print('DB Conn was successfull!')
-        break
-    except Exception as error:
-        print(f"Error: {error}")
-        time.sleep(2)
-
-models.Base.metadata.create_all(bind=engine)
-get_db()
+# while True:
+#     try:
+#         conn = psycopg2.connect(host=HOST, database=DATABASE,
+#                                 user=USER, password=PASSWORD, cursor_factory=RealDictCursor)
+#         cursor = conn.cursor()
+#         print('DB Conn was successfull!')
+#         break
+#     except Exception as error:
+#         print(f"Error: {error}")
+#         time.sleep(2)
 
 SQLALCHEMY_DATABASE_URL = f'postgresql://{USER}:{PASSWORD}@{HOST}/{DATABASE}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 
@@ -48,3 +41,6 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+get_db()
--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from .routers import post, user, auth
+from .database import engine
+from . import models
 
 app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
 
 
 # Load routers
