064e731e44bf1915635e47f748a82b9cad25a9a4
Aditya Gundecha <adityagundecha@hotmail.com>
2022-01-16 22:02:15 +0530

    Adding post id check in vote router

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -1,3 +1,4 @@
+from pyexpat import model
 from app import oauth2
 from fastapi import APIRouter, HTTPException, status
 from fastapi.params import Depends
@@ -15,6 +16,11 @@ router = APIRouter(
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 def vote(vote: schemas.Vote, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+
+    db.query(models.Post).filter(models.Post.id == vote.post_id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Post with id {vote.post_id} does not exist')
     vote_query = db.query(models.Vote).filter(
         models.Vote.post_id == vote.post_id, models.Vote.user_id == current_user.id)
     found_vote = vote_query.first()
