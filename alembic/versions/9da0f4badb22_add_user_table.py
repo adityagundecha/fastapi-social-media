638d4d50faaccbb53e2d5ec82cba3754854f2585
Aditya Gundecha <adityagundecha@hotmail.com>
2022-01-21 22:32:18 +0530

    Adding user table in alembic

---
+++ alembic/versions/9da0f4badb22_add_user_table.py
@@ -0,0 +1,32 @@
+"""add user table
+
+Revision ID: 9da0f4badb22
+Revises: 954ee4e7fddc
+Create Date: 2022-01-21 22:06:23.203534
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9da0f4badb22'
+down_revision = '954ee4e7fddc'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table('users',
+                    sa.Column('id', sa.INTEGER(),
+                              nullable=False, primary_key=True),
+                    sa.Column('email', sa.String(), nullable=False),
+                    sa.Column('password', sa.String(), nullable=False),
+                    sa.Column('created_at', sa.TIMESTAMP(timezone=True), server_default=sa.text('now()'), nullable=False),
+                    sa.PrimaryKeyConstraint('id'),
+                    sa.UniqueConstraint('email')
+                    )
+
+
+def downgrade():
+    pass
